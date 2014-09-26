7bbf459e117574927fe26353254fb89d7b01cf36
Povilas Panavas <povilas.panavas@gmail.com>
2014-09-26 15:09:20 +0100

    First commit includidng all the files and examples

---
+++ CsvToCSharpClassApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CsvToCSharpClass.Library;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CsvToCSharpClass.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cSharpClass = CsvToClass.CSharpClassCodeFromCsvFile(@"..\..\..\ExampleFiles\InputFile.csv", ",", "[DelimitedRecord(\",\")]", "[FieldOptional()]");
+            File.WriteAllText(@"..\..\..\ExampleFiles\OutputFile.cs", cSharpClass);
+        }
+    }
+}
---
+++ ExampleFiles/OutputFile.cs
@@ -0,0 +1,171 @@
+[DelimitedRecord(",")]
+public class MetroBank import data Sample { 
+	[FieldOptional()]
+	public int OrderNo { get; set; }
+
+	[FieldOptional()]
+	public DateTime Date { get; set; }
+
+	[FieldOptional()]
+	public string ProdCode { get; set; }
+
+	[FieldOptional()]
+	public string ProductName { get; set; }
+
+	[FieldOptional()]
+	public string DocumentName { get; set; }
+
+	[FieldOptional()]
+	public int Qty { get; set; }
+
+	[FieldOptional()]
+	public string Price { get; set; }
+
+	[FieldOptional()]
+	public string DeliveryCost { get; set; }
+
+	[FieldOptional()]
+	public int OrderLineID { get; set; }
+
+	[FieldOptional()]
+	public string Status { get; set; }
+
+	[FieldOptional()]
+	public string User { get; set; }
+
+	[FieldOptional()]
+	public string PONo { get; set; }
+
+	[FieldOptional()]
+	public string PONo2 { get; set; }
+
+	[FieldOptional()]
+	public string CostCentre { get; set; }
+
+	[FieldOptional()]
+	public string DeliveryAddress { get; set; }
+
+	[FieldOptional()]
+	public string InvoiceAddress { get; set; }
+
+	[FieldOptional()]
+	public string Usercomments { get; set; }
+
+	[FieldOptional()]
+	public string Column18 { get; set; }
+
+	[FieldOptional()]
+	public string CreditsUsed { get; set; }
+
+	[FieldOptional()]
+	public string DiscountCodeOverOrder { get; set; }
+
+	[FieldOptional()]
+	public string accountcode { get; set; }
+
+	[FieldOptional()]
+	public string Column22 { get; set; }
+
+	[FieldOptional()]
+	public string Column23 { get; set; }
+
+	[FieldOptional()]
+	public string Column24 { get; set; }
+
+	[FieldOptional()]
+	public string Column25 { get; set; }
+
+	[FieldOptional()]
+	public string Column26 { get; set; }
+
+	[FieldOptional()]
+	public string Column27 { get; set; }
+
+	[FieldOptional()]
+	public string Column28 { get; set; }
+
+	[FieldOptional()]
+	public string Column29 { get; set; }
+
+	[FieldOptional()]
+	public string Column30 { get; set; }
+
+	[FieldOptional()]
+	public string Column31 { get; set; }
+
+	[FieldOptional()]
+	public string Column32 { get; set; }
+
+	[FieldOptional()]
+	public string Column33 { get; set; }
+
+	[FieldOptional()]
+	public string Column34 { get; set; }
+
+	[FieldOptional()]
+	public string Column35 { get; set; }
+
+	[FieldOptional()]
+	public string Column36 { get; set; }
+
+	[FieldOptional()]
+	public string Column37 { get; set; }
+
+	[FieldOptional()]
+	public string Column38 { get; set; }
+
+	[FieldOptional()]
+	public string Column39 { get; set; }
+
+	[FieldOptional()]
+	public string Column40 { get; set; }
+
+	[FieldOptional()]
+	public string Column41 { get; set; }
+
+	[FieldOptional()]
+	public string Column42 { get; set; }
+
+	[FieldOptional()]
+	public string Column43 { get; set; }
+
+	[FieldOptional()]
+	public string Column44 { get; set; }
+
+	[FieldOptional()]
+	public string Column45 { get; set; }
+
+	[FieldOptional()]
+	public string Column46 { get; set; }
+
+	[FieldOptional()]
+	public string Column47 { get; set; }
+
+	[FieldOptional()]
+	public string Column48 { get; set; }
+
+	[FieldOptional()]
+	public string Column49 { get; set; }
+
+	[FieldOptional()]
+	public string Column50 { get; set; }
+
+	[FieldOptional()]
+	public string Column51 { get; set; }
+
+	[FieldOptional()]
+	public string Column52 { get; set; }
+
+	[FieldOptional()]
+	public string Column53 { get; set; }
+
+	[FieldOptional()]
+	public string Column54 { get; set; }
+
+	[FieldOptional()]
+	public string Column55 { get; set; }
+
+	[FieldOptional()]
+	public string DiscountValue { get; set; }
+
+}
